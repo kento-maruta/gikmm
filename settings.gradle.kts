e5b25dc706c79220c2177ab34d4470e4702ae9b7
kento.maruta <kento.maruta@gigbase.jp>
2023-01-04 18:22:31 +0900

    調整

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,12 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
+    plugins {
+        id("com.android.application")  version "7.3.1" apply false
+        id("com.android.library")  version "7.3.1" apply false
+        kotlin("android") version "1.7.10" apply false
+        kotlin("multiplatform") version "1.7.10" apply false
+    }
 }
 
 dependencyResolutionManagement {
@@ -14,4 +20,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "gigkmm"
+include(":app")
 include(":shared")
