cc5161c014f1efef0f790f48afbb72ebb8882673
kento.maruta <kento.maruta@gigbase.jp>
2023-01-05 12:13:12 +0900

    調整

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -21,9 +21,6 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
-                implementation("io.ktor:ktor-client-json:$ktorVersion")
-                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
             }
         }
